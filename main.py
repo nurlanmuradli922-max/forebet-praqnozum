8967a4aeaf65ae3e34413f6810eecddde778a68c
nurlanmuradli922-max <nurlanmuradli922@gmail.com>
2026-09-09 03:32:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
-
-
-
-
-
+import requests
+from datetime import datetime
+url="https://www.forebet.com/en/football-tips-and-predictions-for-today"
+h={"User-Agent":"Mozilla/5.0"}
+:t=requests.get(url,headers=h,timeout=30).text
+:open("README.md","w",encoding="utf-8).write(f"#Proqnoz{datetime.now()}\n\n{t[:2000]}")
